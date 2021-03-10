a5a8ddf841a175b9a8aa47c2a81216865e9253c3
Daniel2711 <detaddeo.d@gmail.com>
2021-03-10 14:36:57 +0100

    Aggiunti percorsi root

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('pagina-home');
+
+Route::get('/news', function () {
+    return view('news');
+})->name('pagina-news');
+
+Route::get('/products', function () {
+    return view('products');
+})->name('pagina-prodotti');
