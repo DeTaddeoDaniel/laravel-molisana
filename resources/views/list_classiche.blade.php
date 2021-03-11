b857e13c3e4450cb035284d5d0488bed292759b9
Daniel2711 <detaddeo.d@gmail.com>
2021-03-11 17:30:08 +0100

    Pasta classica suddiviso per lunghezza

--- resources/views/list_classiche.blade.php
+++ resources/views/list_classiche.blade.php
@@ -3,26 +3,79 @@
 @section('title', 'Molisana classiche')
 
 @section('content')
-    <section>
-        <div class="container">
+    <section class="bg-azzurro">
+        <div class="container ">
             
+            @php
+                $collection = collect($paste);
+                $pasta_lunga = $collection -> where('tipo','lunga');
+                $pasta_corta = $collection -> where('tipo','corta');
+                $pasta_cortissima = $collection -> where('tipo','cortissima');
 
+                // dd($pasta_lunga);
+
+            @endphp       
+
+            {{-- PASTA: LUNGHE --}}
+            <h2>LE LUNGHE</h2>
+            
             <div class="cards">
 
-                @php
-                    // dd($paste);
-                @endphp       
+                @foreach ($pasta_lunga as $pasta) 
 
-
-                @foreach ($paste as $pasta)
                     <article class="card">
                         <a href="#">
                             <img src="{{asset($pasta['src'])}}" alt="$tipo_pasta['link']">
                         </a>
                     </article>
+
+                    {{-- lunga / corta / cortissima --}}
+
                 @endforeach        
                     
             </div>
+            
+            {{-- PASTA: LE CORTE --}}
+            <h2>LE corte</h2>
+            
+            <div class="cards">
+
+                @foreach ($pasta_corta as $pasta) 
+
+                    <article class="card">
+                        <a href="#">
+                            <img src="{{asset($pasta['src'])}}" alt="$tipo_pasta['link']">
+                        </a>
+                    </article>
+
+                    {{-- lunga / corta / cortissima --}}
+
+                @endforeach        
+                    
+            </div>
+
+            {{-- PASTA: LE CORTISSIME --}}
+            <h2>LE cortissima</h2>
+            
+            <div class="cards">
+
+                @foreach ($pasta_corta as $pasta) 
+
+                    <article class="card">
+                        <a href="#">
+                            <img src="{{asset($pasta['src'])}}" alt="$tipo_pasta['link']">
+                        </a>
+                    </article>
+
+                    {{-- lunga / corta / cortissima --}}
+
+                @endforeach        
+                    
+            </div>
+
+            
+
+
         </div>
     </section>
 @endsection
